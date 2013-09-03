2a88bc61b80ea683cd4369af9cf5cc90e9bc606f
Birkir Rafn Gudjonsson <birkir.gudjonsson@gmail.com>
2013-09-03 12:34:58 +0200

    Added bootstrap

---
+++ media/js/application.js
@@ -0,0 +1,16 @@
+var application = (function () {
+
+	var app = {};
+
+	// doc ready callback
+	app.ready = function () {
+
+		
+
+		return;
+	};
+
+	return app;
+}());
+
+$(document).on('ready', application.ready);
